a355f07008e93fe95328e28a8226e467403b2445
alan araújo <ag23672311@gmail.com>
2026-07-06 15:20:50 -0300

    feat: configura variaveis de ambiente e corrige tipos do typescript

--- server.ts
+++ server.ts
@@ -1,16 +1,21 @@
 const express = require('express');
-import type {Request, Response } from 'express';
 const mongoose = require('mongoose');
-import cors =require('cors');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const MONGO_URI = 'mongodb+srv://admin:qwe123@cluster0.l2mgk4h.mongodb.net/barbearia?retryWrites=true&w=majority&appName=Cluster0';
+const databaseUri = process.env.MONGODB_URI;
 
-mongoose.connect(MONGO_URI)
+if (!databaseUri) {
+    console.error("Erro: A variável MONGODB_URI não foi definida no arquivo .env");
+    process.exit(1);
+}
+
+mongoose.connect(databaseUri)
 .then(() => console.log(' 🔥Conectado com sucesso ao MongoLocal!'))
 .catch((err: any) => console.error('Erro de conexão com o MongoDB:', err));
 
@@ -24,16 +29,16 @@ const TransactionsSchema = new mongoose.Schema({
     const TransactionModel  = mongoose.model('Transaction', TransactionsSchema);
 
     /*--RotasdaAPI----*/
-    app.get('/api/transactions', async (req: Request, res: Response) => {
+    app.get('/api/transactions', async (req: any, res: any) => {
         try {
             const transaction = await TransactionModel.find().sort({ date: -1 });
-            res.json(transaction);
-        } catch (error) {
+            res.json({data: transaction});
+        } catch (error: any) {
             res.status(500).json({message: 'Erro ao buscar dados.'});
         }
         });
 
-        app.post('/api/transactions', async (req: Request, res: Response) => {
+        app.post('/api/transactions', async (req: any, res: any) => {
             try {
                 const { description, amount, type } = req.body;
                 const newTransaction = new TransactionModel({ description, amount, type });
