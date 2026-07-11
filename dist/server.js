db754cc1e22f1b48267f5bbc36e40685ce55ba20
alan araújo <ag23672311@gmail.com>
2026-07-11 17:19:26 -0300

    feat: adiciona rota delete

--- dist/server.js
+++ dist/server.js
@@ -43,6 +43,19 @@ app.post('/api/transactions', async (req, res) => {
         res.status(400).json({ message: 'Erro ao salvar dados.' });
     }
 });
+app.delete('/api/transactions:/id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedTransaction = await TransactionModel.findByIdAndDelete(id);
+        if (!deletedTransaction) {
+            return res.status(404).json({ message: 'Transação não encontrada.' });
+        }
+        return res.status(200).json({ message: 'Transação deletada com sucesso!' });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Erro ao deletar dados do servidor.' });
+    }
+});
 app.listen(5000, () => {
     console.log('🚀 API escutando redondo na porta 5000');
 });
